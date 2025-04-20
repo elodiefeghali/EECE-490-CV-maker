e06011edb53c6835dd178a6b14af6e2e99ebb63a
elodiefeghali <elodie.feghali@gmail.com>
2025-04-20 16:16:30 +0300

    Update app.py

--- Models/Model 2/app.py
+++ Models/Model 2/app.py
@@ -3,3 +3,10 @@ import re
 import joblib
 import numpy as np
 from sklearn.datasets import load_files
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV, cross_val_score
+from sklearn.metrics import classification_report
+
+
