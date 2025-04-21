b26e0c0bba8d12f56a0cf7bbe2053673e38aebad
elodiefeghali <elodie.feghali@gmail.com>
2025-04-21 18:10:53 +0300

    Update app.py

--- Models/Model 2/app.py
+++ Models/Model 2/app.py
@@ -5,6 +5,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+
 # Step 1: Load the dataset
 file_path = '/mnt/data/job_recommendation_dataset 3.csv'
 df = pd.read_csv(file_path)
